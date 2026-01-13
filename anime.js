b649d087de8e227b6e2e1c4e60dc2c41ff9771d7
J-S-YT <josestatusyt@gmail.com>
2026-01-12 21:05:09 -0300

    teste final

--- anime.js
+++ anime.js
@@ -1,3 +1,27 @@
+let captchaValido = false;
+let tokenCaptcha = null;
+
+function captchaSucesso() {
+  captchaValido = true;
+  tokenCaptcha = crypto.randomUUID(); // token único
+  document.getElementById("btnContinuar").style.display = "inline-block";
+  document.getElementById("btnContinuar").dataset.token = tokenCaptcha;
+}
+
+const observer = new MutationObserver(() => {
+  const captcha = document.querySelector(".cf-turnstile");
+
+  if (!captcha && !captchaValido) {
+    alert("🚫 Manipulação detectada.");
+    location.reload();
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
 (function (s) {
   (s.dataset.zone = "10455054"),
     (s.src = "https://gizokraijaw.net/vignette.min.js");
